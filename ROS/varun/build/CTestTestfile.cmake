255f43eade7cbf85a2ecd0b4e27158737e8a35ba
varunszapp <varunszapp@gmail.com>
2017-02-04 03:46:55 +0530

    Workspace Initialization

---
+++ ROS/varun/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/varun/git/HURO-2017/ROS/varun/src
+# Build directory: /home/varun/git/HURO-2017/ROS/varun/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ ROS/varun/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/varun/git/HURO-2017/ROS/varun/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
