4d39d7396714c7064c1a23f7332406e1560120fc
shashikg <shashikg@iitk.ac.in>
2017-02-03 21:49:27 +0530

    Workspace Initialization

---
+++ ROS/shashi/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/shashi/Desktop/HURO-2017/ROS/shashi/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/shashi/Desktop/test_ws/devel;/home/shashi/Desktop/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/shashi/Desktop/test_ws/devel;/home/shashi/Desktop/catkin_ws/devel;/opt/ros/kinetic" != "" else []
