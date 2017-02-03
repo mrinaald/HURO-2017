359cd332b3e1b48d6db2db2838dd22c283639101
Mrinaal Dogra <mrinaal@iitk.ac.in>
2017-02-03 21:54:22 +0530

    Workspace Initialization

---
+++ ROS/mrinaal/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/mrinaal/git/HURO-2017/ROS/mrinaal/src
+# Build directory: /home/mrinaal/git/HURO-2017/ROS/mrinaal/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ ROS/mrinaal/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/mrinaal/git/HURO-2017/ROS/mrinaal/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/mrinaal/git/HURO-2017/ROS/mrinaal/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/mrinaal/git/HURO-2017/ROS/mrinaal/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/mrinaal/git/HURO-2017/ROS/mrinaal/src:$ROS_PACKAGE_PATH"
